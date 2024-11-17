ec16c89b39c3a6abff36465f97632c701ded7deb
Tiara Oktavian <tiaraoktaviani40@gmail.com>
2024-11-18 02:05:33 +0700

    feat: enum class fore role user

--- src/main/java/com/enigma/ChopChop/constant/UserRole.java
+++ src/main/java/com/enigma/ChopChop/constant/UserRole.java
@@ -1,4 +1,24 @@
 package com.enigma.ChopChop.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
+    ROLE_ADMIN("admin"),
+    ROLE_USER("user");
+
+    private final String Description;
+
+    UserRole(String description) {
+        this.Description = description;
+    }
+
+    public static UserRole findByDescription(String description) {
+        for (UserRole role : values()) {
+            if (role.Description.equalsIgnoreCase(description)) {
+                return role;
+            }
+        }
+        return null;
+    }
 }
