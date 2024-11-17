63e43ce6fc196a36e1f0a7da4b9a20e3879e988b
Tiara Oktavian <tiaraoktaviani40@gmail.com>
2024-11-17 20:07:24 +0700

    first init

---
+++ src/main/java/com/enigma/ChopChop/ChopChopApplication.java
@@ -0,0 +1,11 @@
+package com.enigma.ChopChop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopChopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChopChopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/enigma/ChopChop/ChopChopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.ChopChop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopChopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
